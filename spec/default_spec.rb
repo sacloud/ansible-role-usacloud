b19b8ffe89c20d9da46719fa3c89a28de6011515
wate <pc.poisoning@gmail.com>
2017-11-19 20:37:49 +0900

    ansible galaxy用に調整

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -1,11 +1,9 @@
 require 'spec_helper'
 
-describe 'role usacloud' do
-  describe yumrepo('usacloud') do
-    it { should exist }
-    it { should be_enabled }
-  end
-  describe package('usacloud') do
-    it { should be_installed }
-  end
+describe yumrepo('usacloud') do
+  it { should exist }
+  it { should be_enabled }
+end
+describe package('usacloud') do
+  it { should be_installed }
 end
